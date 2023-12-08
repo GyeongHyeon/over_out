1c5015a4f3bdd85724f2b47b31a4e3a80c1a6afe
magumagu <powerofman26@gmail.com>
2023-12-08 15:42:57 +0900

    20231208_코드수정

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 (() => {
+  HTMLAudioElement.prototype.rewindAndPlay = function () { this.pause(); this.currentTime = 0; this.play();}
+  
   let selectedIndex = -1; // 초기화, 초기값은 -1;
   let time ;
   const listItems = document.querySelectorAll('.list_item');
@@ -6,61 +8,72 @@
   const listContainer = document.querySelector('.list_container');
   const announcer = document.querySelector('#announcer');
   const turnMainBtn = document.querySelector('.turn_main');
-  const Bgms = [new Audio('bgm/Confirm.mp3'), new Audio('bgm/select.mp3'), new Audio('bgm/background.mp3')];
+  const sounds = {
+    confirm:new Audio('bgm/Confirm.mp3'), 
+    select:new Audio('bgm/select.mp3'),
+    background:new Audio('bgm/background.mp3'),
+    back:new Audio('bgm/back.mp3')
+  };
   const Head = document.querySelector("header");
 
 // 버튼 생성 클래스
 class Button {
-  constructor(text, onClick, backgroundColor, gradientValue, padding, position, top, right, fontWeight, targetTagId) {
-    this.text = text;
+  /** 
+   * @param {string} text
+   * @param {(e?:MouseEvent)=>void} onClick
+   * @param {CSSStyleDeclaration?} style
+   */ 
+  constructor(text, onClick, style = {}) {
+    this.button = document.createElement('button');
     this.onClick = onClick;
-    this.backgroundColor = backgroundColor;
-    this.gradientValue = gradientValue;
-    this.padding = padding;
-    this.position = position;
-    this.top = top;
-    this.right = right;
-    this.targetTagId = targetTagId;
-    this.fontWeight = fontWeight;
-    this.button = null;
+    this.text = text;
+    this.style = style;
+    
   }
-
-  render() {
-    this.button = document.createElement("button");
-    this.button.textContent = this.text;
-    this.button.style.backgroundColor = this.backgroundColor;
-    this.button.style.backgroundImage = this.gradientValue;
-    this.button.style.padding = this.padding;
-    this.button.style.position = this.position;
-    this.button.style.top = this.top;
-    this.button.style.right = this.right;
-    this.button.style.fontWeight = this.fontWeight;
+  /** @param {string} parentElementSelector 버튼을 렌더링할 부모 요소 셀렉터 string을 전달 */
+  render(parentElementSelector = "body") {
     this.button.addEventListener("click", this.onClick);
     this.button.addEventListener("keydown", (event) => {
-      if (event.key === "Enter" || event.key === " ") {
+      if (/^(\ |Enter)$/.test(event.key)) {
         this.onClick();
       }
     });
 
-    const targetTag = document.getElementById(this.targetTagId);
-    if (targetTag) {
-      targetTag.appendChild(this.button);
-    } else {
-      document.body.appendChild(this.button);
-    }
+    document.querySelector(parentElementSelector).append(this.button);
+  }
+  
+  
+  /** @return {CSSStyleDeclaration} 버튼 인라인 스타일 객체를 가져옴 */
+  get style(){ return this.button.style; }
+  
+  /** @param {CSSStyleDeclaration?} style 버튼 인라인 스타일을 객체 형태로 여러 프로퍼티를 설정함. 기존 스타일을 없애지 않고 전달받은 객체 프로퍼티만 바꿈. */ 
+  set style(style){ 
+    Object.assign(this.style, style);
+  }
+
+  /** @param {string} text */
+  set text(text){
+    this.button.textContent = text;
+  }
+  /** @return {string} */
+  get text() {
+    this.button.textContent;
   }
 }
 
 // 페이지 이동 버튼
-const pageLinkButton = new Button(" ", () => {
-  Bgms[0].play();
-  const currentUrl = window.location.href;
-  if (currentUrl.includes("index.html")) {
-    window.location.href = "sub_page_1.html";
-  } else if (currentUrl.includes("sub_page_1.html")) {
-    window.location.href = "index.html";
-  }
-}, " ", "linear-gradient(to top, #4A564A, #00cc00)", "10px", "absolute", "bold", "0px", "0px", "hyper_portal");
+const pageLinkButton = new Button(" ",() => {
+    sounds.confirm.rewindAndPlay();
+    const currentUrl = window.location.href;
+    if (currentUrl.includes("index.html")) {
+      window.location.href = "sub_page_1.html";
+    } else if (currentUrl.includes("sub_page_1.html")) {
+      window.location.href = "index.html";
+    }
+  },
+  {backgroundColor:"transparent", backgroundImage:"linear-gradient(to top, #4A564A, #00cc00)",
+  padding:"10px", position:"absolute", fontWeight:"bold", right:"0px", top:"0px"}
+);
 
 // 현재 페이지 URL에 따라 버튼 텍스트 변경
 const currentUrl = window.location.href;
@@ -71,14 +84,14 @@ if (currentUrl.includes("sub_page_1.html")) {
 
 }
 
-pageLinkButton.render();
+pageLinkButton.render("#hyper_portal");
 
   
   // 배경음OFF 버튼
   const myButton = new Button("배경음OFF", () => {
-    Bgms[2].pause();
-    Bgms.pop();
-  }, "#f00", "10px",  "fixed", "bold", "main_container");
+    sounds.background.pause();
+    delete sounds.background;
+  }, {backgroundColor:"#f00", padding:"10px",  position:"fixed", fontWeight:"bold"});
 
   if (window.innerWidth < 500) {
     myButton.render();
@@ -99,7 +112,7 @@ pageLinkButton.render();
 
   listItems.forEach((item, index) => { // 항목마다 이벤트 등록
     item.addEventListener('click', () => {
-      Bgms[0].play();
+      sounds.confirm.play();
       mainContents[index].classList.add('show_content');
       turnMainBtn.classList.add('show_btn');
       listContainer.classList.add('list_container_hid');
@@ -108,6 +121,7 @@ pageLinkButton.render();
 
     item.addEventListener('mouseover', () => {
       select(index);
+      sounds.select.rewindAndPlay();
     }); // 마우스를 항목에 올리면 해당 항목을 선택함.
 
     item.addEventListener('keydown', (event) => {
@@ -116,19 +130,19 @@ pageLinkButton.render();
 
       switch (key) { // key 조건에 따라 다음을 실행
         case "ArrowUp":
-          Bgms[1].play();
-          Bgms[1].currentTime = 0;
+          sounds.select.play();
+          sounds.select.currentTime = 0;
           select(selectedIndex > 0 ? selectedIndex - 1 : lastIndex); // 0보다 선택된 인덱스가 크면 1을 빼고, 많거나 같으면 마지막 인덱스로 설정
            break;
         case 'ArrowDown':
-          Bgms[1].play();
-          Bgms[1].currentTime = 0;
+          sounds.select.play();
+          sounds.select.currentTime = 0;
           select(selectedIndex < lastIndex ? selectedIndex + 1 : 0); // 선택된 인덱스가 마지막 인덱스보다 작으면 1을 더하고, 그렇지 않으면 0으로 설정
           break;
         case 'Enter':
         case ' ': 
-          Bgms[0].play();
-          Bgms[0].currentTime = 0;
+          sounds.confirm.play();
+          sounds.confirm.currentTime = 0;
           mainContents[index].classList.toggle('show_content');
           turnMainBtn.classList.add('show_btn');
           listContainer.classList.toggle('list_container_hid');
@@ -145,7 +159,7 @@ pageLinkButton.render();
   
 
   if (windowWidth <= 500) {
-    Bgms[2].play();
+    sounds.background.play();
     pElements.forEach((p) => {
       p.setAttribute('tabindex', '0');
     });
@@ -180,8 +194,9 @@ pageLinkButton.render();
   });
 
   function toggleShowTable(index) {
-    Bgms[0].play();
-    Bgms[0].currentTime = 0;
+    sounds.confirm.play();
+
+    sounds.confirm.currentTime = 0;
     if (!tableBox[index].classList.contains('show_table')) {
       tableBox[index].classList.add('show_table');
       tableBox[index].focus();
@@ -195,12 +210,11 @@ pageLinkButton.render();
   }
      
 
- //메인화면으로 이동
- const turnMainButton = document.querySelector('.turn_main');//'처음으로' 버튼
+  //메인화면으로 이동
+  const turnMainButton = document.querySelector('.turn_main');//'처음으로' 버튼
 
- turnMainButton.addEventListener('click', () => {
-  Bgms[0].play();
-  Bgms[0].currentTime = 0;
+  turnMainButton.addEventListener('click', () => {
+  sounds.confirm.rewindAndPlay();
   tableBox.forEach((BoX) => {
     if(BoX.classList.contains('show_table')){
     BoX.classList.remove('show_table');}
@@ -211,6 +225,8 @@ pageLinkButton.render();
     listContainer.classList.remove('list_container_hid');
     turnMainBtn.classList.remove('show_btn');
     announceForSR('메인화면임');
+    document.querySelector(".focusing").focus();
+    sounds.back.rewindAndPlay();
  });
 
  // 상태를 음성으로 알리는 함수
@@ -223,18 +239,23 @@ pageLinkButton.render();
   };
 
   document.addEventListener("mousemove", () => {
-    Bgms[2].play();
+    sounds.background.play();
   });
+
+  document.addEventListener("focusin",(evt)=>{
+    if(evt.isTrusted){
+      sounds.select.rewindAndPlay();
+    }
+  })
   
   document.addEventListener("keydown", (event) => {
-    if (event.key === 'Tab') {
-      Bgms[2].play();
-    }else if(event.key === 'Escape'){
-      Bgms[2].pause();
-      Bgms.pop();
+    if(event.key === 'ArrowLeft'){
+      turnMainBtn.click();
+    } else if(event.key === 'Escape'){
+      sounds.background.pause();
+      delete sounds.background;
     }
   });
 
-  
-
+ 
 })();
